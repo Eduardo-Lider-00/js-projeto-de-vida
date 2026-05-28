26ee28d031baa462ba1d4fa97ae4bf1a36258d16
bigode <eduardo.caetano.araujo@escola.pr.gov.br>
2026-05-27 22:06:16 -0300

    contador tempo3

--- main.js
+++ main.js
@@ -26,8 +26,8 @@ function calculaTempo(tempoObjetivo) {
     let dias = Math.floor(horas / 24);
 
     segundos %= 60;
-    minutos%= 60;
-    horas%= 24;
+    minutos %= 60;
+    horas %= 24;
 
     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos.";
 
